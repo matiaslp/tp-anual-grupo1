3c5156729d29385aa667761bb284da61519d2154
Matías Schaab <schaab.matias@gmail.com>
2016-09-08 01:42:39 -0300

    Completados los test de Acciones

--- src/test/java/test_miscellaneous/TestAcciones.java
+++ src/test/java/test_miscellaneous/TestAcciones.java
@@ -2,7 +2,9 @@ package test_miscellaneous;
 
 import java.util.HashMap;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 import autentification.Accion;
 import autentification.AuthAPI;
@@ -11,21 +13,48 @@ import autentification.Usuario;
 
 public class TestAcciones {
 
-	private Usuario prueba;
+	private Usuario user;
 	private AuthAPI Autenticador;
 	
 	@Before
 	public void init(){
-		prueba = new Usuario();
-		prueba.setID(1);
-		prueba.setPassword("password");
-		prueba.setUsername("usuario");
-		prueba.setRol(Rol.ADMIN);
-		prueba.setFuncionalidades(new HashMap<String, Accion>());
-		prueba.getFuncionalidades().put("enviarMail", AuthAPI.Acciones.get("enviarMail"));
+		Autenticador = AuthAPI.getInstance();
+		
+		user = new Usuario();
+		user.setID(1);
+		user.setPassword("password");
+		user.setUsername("usuario");
+		user.setRol(Rol.ADMIN);
+		user.setFuncionalidades(new HashMap<String, Accion>());
+		
+		user.getFuncionalidades().put("enviarMail", AuthAPI.Acciones.get("enviarMail"));
 		
 		Autenticador = AuthAPI.getInstance();
 		
-		Autenticador.getListaUsuarios().add(prueba);
+		Autenticador.getListaUsuarios().add(user);
 	}
+	
+	@Test
+	public void testAgregarUnaAccion(){
+		Autenticador.agregarFuncionalidad("unaFuncionalidad", user);
+		Assert.assertEquals(user.getFuncionalidades().size(), 2);
+	}
+	
+	@Test
+	public void testAgregarExistente(){
+		Assert.assertFalse(Autenticador.agregarFuncionalidad("enviarMail", user));
+		Assert.assertEquals(user.getFuncionalidades().size(), 1);
+	}
+	
+	@Test
+	public void testSacarFuncionalidad(){
+		Autenticador.sacarFuncionalidad("enviarMail", user);
+		Assert.assertEquals(user.getFuncionalidades().size(), 0);
+	}
+	
+	@Test
+	public void testChequeoFuncionalidad(){
+		Assert.assertTrue(Autenticador.chequearFuncionalidad("enviarMail", user));
+	}
+	
 }
