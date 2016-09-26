672d71bf096f4b218c759c847cac3453dadd9d96
Matías Schaab <schaab.matias@gmail.com>
2016-09-26 12:59:29 -0300

    Minor Fix
    Me olvidé de descomentar algo que habia dejado para testear

--- src/main/java/autentification/funciones/FuncAgregarAcciones.java
+++ src/main/java/autentification/funciones/FuncAgregarAcciones.java
@@ -18,21 +18,21 @@ public class FuncAgregarAcciones extends Accion {
 		isProcess = true;
 	}
 
-//	public void agregarAcciones(Usuario user, String Token, int cantidadReintentos, boolean enviarEmail,
-//			boolean disableAccion, String filePath, Usuario unUser) {
-//		if (validarsesion(user, Token)) {
-//			AgregarAcciones proceso = new AgregarAcciones(cantidadReintentos, enviarEmail, disableAccion, filePath, unUser);
-//			proceso.execute();
-//		}
-//	}
-//
-//	// creacion Proceso para agregar a la lista en Proceso Multiple
-//	public Proceso prepAgregarAcciones(Usuario user, String Token, int cantidadReintentos, boolean enviarEmail,
-//			boolean disableAccion, String filePath, Usuario unUser) {
-//		if (validarsesion(user, Token)) {
-//			return new AgregarAcciones(cantidadReintentos, enviarEmail, disableAccion, filePath, unUser);
-//		} else
-//			return null;
-//	}
+	public void agregarAcciones(Usuario user, String Token, int cantidadReintentos, boolean enviarEmail,
+			boolean disableAccion, String filePath, Usuario unUser) {
+		if (validarsesion(user, Token)) {
+			AgregarAcciones proceso = new AgregarAcciones(cantidadReintentos, enviarEmail, disableAccion, filePath, unUser);
+			proceso.execute();
+		}
+	}
+
+	// creacion Proceso para agregar a la lista en Proceso Multiple
+	public Proceso prepAgregarAcciones(Usuario user, String Token, int cantidadReintentos, boolean enviarEmail,
+			boolean disableAccion, String filePath, Usuario unUser) {
+		if (validarsesion(user, Token)) {
+			return new AgregarAcciones(cantidadReintentos, enviarEmail, disableAccion, filePath, unUser);
+		} else
+			return null;
+	}
 
 }
