cd236e7c76449fa7d8b17c768a2fef6621e8dd39
bruno <bisioba29@gmail.com>
2016-10-18 18:56:19 -0300

    test ActualizarLocalComercial

--- src/test/java/abmc/TestABMC_Consulta.java
+++ src/test/java/abmc/TestABMC_Consulta.java
@@ -11,7 +11,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import abmc.POI_ABMC;
 import db.DB_POI;
 import poi.Banco;
 import poi.CGP;
@@ -37,54 +36,40 @@ public class TestABMC_Consulta {
 		banco.setCallePrincipal("Alberdi");
 		banco.setCalleLateral("Escalada");
 		ServicioAPI = "http://trimatek.org/Consultas/";
+
+		instance.agregarPOI(cgp);
+		instance.agregarPOI(parada);
+		instance.agregarPOI(local);
+		instance.agregarPOI(banco);
+
 	}
 
 	@Test
 	public void testConsultaVacia() throws JSONException, MalformedURLException, IOException, MessagingException {
 		ArrayList<POI> lista = null;
-		instance.agregarPOI(cgp);
-		instance.agregarPOI(parada);
-		instance.agregarPOI(local);
-		instance.agregarPOI(banco);
-
 		lista = abmc.buscar(ServicioAPI, "", 1);
 		Assert.assertTrue(lista.isEmpty());
-
 	}
 
 	@Test
 	public void testConsultaLocal() throws JSONException, MalformedURLException, IOException, MessagingException {
 		ArrayList<POI> lista = null;
-		instance.agregarPOI(cgp);
-		instance.agregarPOI(parada);
-		instance.agregarPOI(local);
-		instance.agregarPOI(banco);
-
 		lista = abmc.buscar("", "Alberdi", 1);
 		Assert.assertTrue(lista.size() == 1);
-
 	}
 
 	@Test
 	public void testConsultaLocal2() throws JSONException, MalformedURLException, IOException, MessagingException {
 		ArrayList<POI> lista = null;
-		instance.agregarPOI(cgp);
-		instance.agregarPOI(parada);
-		instance.agregarPOI(local);
-		instance.agregarPOI(banco);
-
 		lista = abmc.buscar("", "Mataderos", 1);
 		Assert.assertTrue(lista.size() == 2);
-
 	}
 
 	@Test
 	public void testConsultaRemota() throws JSONException, MalformedURLException, IOException, MessagingException {
 		ArrayList<POI> lista = null;
-
 		lista = abmc.buscar(ServicioAPI, "Mataderos", 1);
 		Assert.assertTrue(lista.size() == 15);
-
 	}
 
 	// deberia devolver 1 solo resultado, pero como el servicio remoto
@@ -93,49 +78,30 @@ public class TestABMC_Consulta {
 	@Test
 	public void testConsultaRemota2() throws JSONException, MalformedURLException, IOException, MessagingException {
 		ArrayList<POI> lista = null;
-
 		lista = abmc.buscar(ServicioAPI, "Galicia", 1);
 		Assert.assertTrue(lista.size() == 16);
-
 	}
 
 	@Test
 	public void testConsultaRemotaVariasPalabras()
 			throws JSONException, MalformedURLException, IOException, MessagingException {
 		ArrayList<POI> lista = null;
-
 		lista = abmc.buscar(ServicioAPI, "Galicia Mataderos", 1);
 		Assert.assertTrue(lista.size() == 16);
-
 	}
 
 	@Test
 	public void testConsulta() throws JSONException, MalformedURLException, IOException, MessagingException {
 		ArrayList<POI> lista = null;
-
-		instance.agregarPOI(cgp);
-		instance.agregarPOI(parada);
-		instance.agregarPOI(local);
-		instance.agregarPOI(banco);
-
 		lista = abmc.buscar(ServicioAPI, "Galicia", 1);
 		Assert.assertTrue(!(lista.isEmpty()));
-
 	}
 
 	@Test
 	public void testConsultavariasPalabras()
 			throws JSONException, MalformedURLException, IOException, MessagingException {
 		ArrayList<POI> lista = null;
-
-		instance.agregarPOI(cgp);
-		instance.agregarPOI(parada);
-		instance.agregarPOI(local);
-		instance.agregarPOI(banco);
-
 		lista = abmc.buscar(ServicioAPI, "Galicia Mataderos", 1);
 		Assert.assertTrue(lista.size() == 18);
-
 	}
-
 }
--- src/test/java/procesos/TestActualizarLocalComercial.java
+++ src/test/java/procesos/TestActualizarLocalComercial.java
@@ -32,29 +32,33 @@ public class TestActualizarLocalComercial {
 	
 	@Test
 	public void testActualizar(){
-		//Corro el proceso
+		
 		String filePath = (new File (".").getAbsolutePath ())+"/src/test/java/procesos/actualizarLocalesComerciales.txt";
-		//Creo los locales comerciales pero solo agrego el 1, 
-		//esperando que los otros 2 los cree el proceso
+		
+		//Creo un local para corroborar que se actualizan los locales que ya existen:
 		LocalComercial local4 = new LocalComercial();
-		LocalComercial local2 = new LocalComercial();
-		LocalComercial local3 = new LocalComercial();
-		
 		local4.setNombre("local1");
-		String[] etiquetas4 = {"matadero", "heladeria"};
-		local4.setEtiquetas(etiquetas4);
-		
-		local2.setNombre("local2");
-		String[] etiquetas2 = {"juegos", "azul", "moron"};
-		local2.setEtiquetas(etiquetas2);
-		
-		local3.setNombre("local3");
-		String[] etiquetas3 = {"azul", "helado", "esquina"};
-		local3.setEtiquetas(etiquetas3);
-		
+		String[] etiquetas = {"matadero", "heladeria"};
+		local4.setEtiquetas(etiquetas);
 		dbPOI.agregarPOI(local4);
-		dbPOI.agregarPOI(local3);
 		
+		//Creo los locales que espero obtener despues de ejecutar el proceso:
+		LocalComercial localEsperado2 = new LocalComercial();
+		localEsperado2.setNombre("local2");
+		String[] etiquetas2 = {"juegos", "azul", "moron"};
+		localEsperado2.setEtiquetas(etiquetas2);
+		
+		LocalComercial localEsperado3 = new LocalComercial();
+		localEsperado3.setNombre("local3");
+		String[] etiquetas3 = {"azul", "helado", "esquina"};
+		localEsperado3.setEtiquetas(etiquetas3);
+		
+		LocalComercial localEsperado4 = new LocalComercial();
+		localEsperado4.setNombre("local4");
+		String[] etiquetas4 = {"pizzeria", "munro", "violeta"};
+		localEsperado4.setEtiquetas(etiquetas4);
+		
+		//Ejecucion del proceso:
 		Usuario admin = DB_Usuarios.getInstance().getUsuarioByName("admin");
 		AuthAPI.getInstance().agregarFuncionalidad("actualizacionLocalesComerciales", admin);
 		String tokenAdmin = AuthAPI.getInstance().iniciarSesion("admin", "123");
@@ -62,13 +66,19 @@ public class TestActualizarLocalComercial {
 		funcion.agregarAcciones(admin, tokenAdmin, 0, false, filePath);
 
 		//Busco las modificaciones para corroborar que se corrio correctamente
-		//POI local1Actualizado = dbPOI.getPOIbyNombre("local1");
 		POI local2Actualizado = dbPOI.getPOIbyNombre("local2");
 		POI local3Actualizado = dbPOI.getPOIbyNombre("local3");
+		POI local4Actualizado = dbPOI.getPOIbyNombre("local4");
 		
-		//Debido a que esta copiando la instancia 
-		//Assert.assertFalse(local1Actualizado.compararEtiquetas(local1));
-		Assert.assertTrue(local2Actualizado.compararEtiquetas(local2));
-		Assert.assertTrue(local3Actualizado.compararEtiquetas(local3));
+		//Compruebo que el local 2 y 3 hayan sido creados:
+		Assert.assertNotNull(local2Actualizado);
+		Assert.assertNotNull(local3Actualizado);
+		
+		//Compruebo que los locales 2 y 3 se hayan creado correctamente:
+		Assert.assertTrue(local2Actualizado.compararEtiquetas(localEsperado2));
+		Assert.assertTrue(local3Actualizado.compararEtiquetas(localEsperado3));
+		
+		//Compruebo que el local 4 se actualizo correctamente:
+		Assert.assertTrue(local4Actualizado.compararEtiquetas(localEsperado4));
 	}
 }
