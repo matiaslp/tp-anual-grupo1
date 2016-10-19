583d8aebcc97907ba017ee2d987501def60f2a3e
matias <matias@localhost.localdomain>
2016-10-19 00:36:30 -0300

    sesion y login in progress

--- src/com/tutorial/LoginBean.java
+++ src/com/tutorial/LoginBean.java
@@ -5,6 +5,11 @@ import javax.faces.application.FacesMessage.Severity;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 
+import ar.edu.utn.dds.grupouno.autentification.AuthAPI;
+import ar.edu.utn.dds.grupouno.autentification.Rol;
+import ar.edu.utn.dds.grupouno.autentification.Usuario;
+import ar.edu.utn.dds.grupouno.autentification.UsuariosFactory;
+import ar.edu.utn.dds.grupouno.db.DB_Usuarios;
 @ManagedBean
 public class LoginBean {
 	private String usuario;
@@ -27,10 +32,34 @@ public class LoginBean {
 	}
 
 	public void login() {
+//		String msg = "";
+//		Severity severity = FacesMessage.SEVERITY_INFO;
+//		if (usuario.equals("dds") && contrasena.equals("dds")) {
+//			msg = "Usuario " + usuario + " autorizado";
+//			FacesContext.getCurrentInstance().getExternalContext().
+//			getSessionMap().put("username", 0);
+//		} else {
+//			msg = "Usuario no autorizado";
+//			severity = FacesMessage.SEVERITY_ERROR;
+//		}
+//		FacesContext.getCurrentInstance().addMessage(null, 
+//				new FacesMessage(severity, msg, null));
+		
 		String msg = "";
 		Severity severity = FacesMessage.SEVERITY_INFO;
-		if (usuario.equals("dds") && contrasena.equals("dds")) {
-			msg = "Usuario " + usuario + " autorizado";			
+		//testTTTTTTTTTTTTTTTTTTT
+		UsuariosFactory fact = new UsuariosFactory();
+		fact.crearUsuario("admin", "pass", Rol.ADMIN);
+		//-------------------------
+		//obtenemos usuario
+		String token = AuthAPI.getInstance().iniciarSesion(usuario, contrasena);
+		if ( token != null) {
+			Usuario user = DB_Usuarios.getInstance().getUsuarioByName(usuario);
+			msg = "Usuario " + usuario + " autorizado";
+			// Iniciamos sesion
+			FacesContext.getCurrentInstance().getExternalContext().
+			getSessionMap().put("username", user.getID());
+			// error de logueo
 		} else {
 			msg = "Usuario no autorizado";
 			severity = FacesMessage.SEVERITY_ERROR;
@@ -42,5 +71,9 @@ public class LoginBean {
 	public void cancelar(){
 		setUsuario(null);
 		setContrasena(null);
+	}
+	
+	public void logout() {
+		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
 	}
 }
