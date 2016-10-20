2bd907b930646e19bae20f4def44456835f18ccc
matias <matias@localhost.localdomain>
2016-10-20 04:19:17 -0300

    fix .setting

--- src/ar/edu/utn/dds/grupouno/login/LoginBean.java
+++ src/ar/edu/utn/dds/grupouno/login/LoginBean.java
@@ -3,6 +3,7 @@ package ar.edu.utn.dds.grupouno.login;
 import javax.faces.application.FacesMessage;
 import javax.faces.application.FacesMessage.Severity;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
 import ar.edu.utn.dds.grupouno.autentification.AuthAPI;
@@ -11,6 +12,7 @@ import ar.edu.utn.dds.grupouno.autentification.Usuario;
 import ar.edu.utn.dds.grupouno.autentification.UsuariosFactory;
 import ar.edu.utn.dds.grupouno.db.DB_Usuarios;
 @ManagedBean
+@SessionScoped
 public class LoginBean {
 	private String usuario;
 	private String contrasena;
@@ -31,25 +33,13 @@ public class LoginBean {
 		this.contrasena = contrasena;
 	}
 
-	public String login() {
-//		String msg = "";
-//		Severity severity = FacesMessage.SEVERITY_INFO;
-//		if (usuario.equals("dds") && contrasena.equals("dds")) {
-//			msg = "Usuario " + usuario + " autorizado";
-//			FacesContext.getCurrentInstance().getExternalContext().
-//			getSessionMap().put("username", 0);
-//		} else {
-//			msg = "Usuario no autorizado";
-//			severity = FacesMessage.SEVERITY_ERROR;
-//		}
-//		FacesContext.getCurrentInstance().addMessage(null, 
-//				new FacesMessage(severity, msg, null));
-		
+	public String login() {	
 		String msg = "";
 		Severity severity = FacesMessage.SEVERITY_INFO;
 		//testTTTTTTTTTTTTTTTTTTT
 		UsuariosFactory fact = new UsuariosFactory();
 		fact.crearUsuario("admin", "pass", Rol.ADMIN);
+		fact.crearUsuario("terminal", "pass", Rol.TERMINAL);
 		//-------------------------
 		//obtenemos usuario
 		String token = AuthAPI.getInstance().iniciarSesion(usuario, contrasena);
@@ -58,7 +48,7 @@ public class LoginBean {
 			msg = "Usuario " + usuario + " autorizado";
 			// Iniciamos sesion
 			FacesContext.getCurrentInstance().getExternalContext().
-			getSessionMap().put("username", user.getID());
+			getSessionMap().put("username", user.getUsername());	
 			return "success";
 			// error de logueo
 		} else {
