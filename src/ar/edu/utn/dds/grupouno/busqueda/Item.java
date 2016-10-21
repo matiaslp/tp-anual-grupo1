128771fc84177922eb85b044caea97663193b45a
matias <matias@localhost.localdomain>
2016-10-20 22:46:04 -0300

    agrego Item.java

---
+++ src/ar/edu/utn/dds/grupouno/busqueda/Item.java
@@ -0,0 +1,16 @@
+package ar.edu.utn.dds.grupouno.busqueda;
+
+public class Item {
+
+	private String value;
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	
+}
