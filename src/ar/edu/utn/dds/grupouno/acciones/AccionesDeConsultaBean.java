6632183e789f530faacd01930fa41bb10dc142f2
bruno <bisioba29@gmail.com>
2016-10-20 23:14:19 -0300

    busqueda

--- src/ar/edu/utn/dds/grupouno/acciones/AccionesDeConsultaBean.java
+++ src/ar/edu/utn/dds/grupouno/acciones/AccionesDeConsultaBean.java
@@ -1,12 +1,10 @@
 package ar.edu.utn.dds.grupouno.acciones;
 
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
-import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 
 import ar.edu.utn.dds.grupouno.autentification.Accion;
@@ -15,31 +13,31 @@ import ar.edu.utn.dds.grupouno.autentification.Usuario;
 import ar.edu.utn.dds.grupouno.db.DB_Usuarios;
 
 @ManagedBean(name="AccionesDeConsultaBean")
-@ViewScoped
+@RequestScoped
 public class AccionesDeConsultaBean {
-	private Set<String> accionesParaSeleccionar = new HashSet<String>();
-	private Set<String> accionesSeleccionadas = new HashSet<String>();
+	private List<String> accionesParaSeleccionar = new ArrayList<String>();
+	private List<String> accionesSeleccionadas = new ArrayList<String>();
 	private String accion = null;
 	
-	@PostConstruct
-    public void init() {
+	@SuppressWarnings("unchecked")
+	public AccionesDeConsultaBean() {
 		String username = ((String) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("username"));
 		String token = ((String) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("token"));
 		Usuario usuario = DB_Usuarios.getInstance().getUsuarioByName(username);
 
-		accionesParaSeleccionar = AuthAPI.getInstance().getAcciones().keySet();
+		accionesParaSeleccionar = (List<String>) AuthAPI.getInstance().getAcciones().keySet();
     }
 	
-	public Set<String> getAccionesParaSeleccionar() {
+	public List<String> getAccionesParaSeleccionar() {
 		return accionesParaSeleccionar;
 	}
-	public void setAccionesParaSeleccionar(Set<String> accionesParaSeleccionar) {
+	public void setAccionesParaSeleccionar(List<String> accionesParaSeleccionar) {
 		this.accionesParaSeleccionar = accionesParaSeleccionar;
 	}
-	public Set<String> getAccionesSeleccionadas() {
+	public List<String> getAccionesSeleccionadas() {
 		return accionesSeleccionadas;
 	}
-	public void setAccionesSeleccionadas(Set<String> accionesSeleccionadas) {
+	public void setAccionesSeleccionadas(List<String> accionesSeleccionadas) {
 		this.accionesSeleccionadas = accionesSeleccionadas;
 	}
 	
