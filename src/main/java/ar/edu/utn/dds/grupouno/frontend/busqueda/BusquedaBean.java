a696560ebe4d7ff151f873b6ba235114c85a3686
matiaslp <matiaslopezpose@gmail.com>
2016-11-21 16:22:03 -0300

    modifico la vista de busqueda para que muestre direccion

--- src/main/java/ar/edu/utn/dds/grupouno/frontend/busqueda/BusquedaBean.java
+++ src/main/java/ar/edu/utn/dds/grupouno/frontend/busqueda/BusquedaBean.java
@@ -5,58 +5,27 @@ import java.util.ArrayList;
 
 import java.util.List;
 
-import javax.annotation.PostConstruct;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.mail.MessagingException;
 
 import org.json.JSONException;
-
-import ar.edu.utn.dds.grupouno.abmc.Busqueda;
 import ar.edu.utn.dds.grupouno.abmc.POI_ABMC;
-import ar.edu.utn.dds.grupouno.abmc.consultaExterna.BusquedaDePOIsExternos;
 import ar.edu.utn.dds.grupouno.autentification.Usuario;
-import ar.edu.utn.dds.grupouno.db.poi.Banco;
 import ar.edu.utn.dds.grupouno.db.poi.POI;
 import ar.edu.utn.dds.grupouno.db.repositorio.Repositorio;
 
 @ManagedBean
 @ApplicationScoped
 public class BusquedaBean {
-	// private int dataTableSize = 1;
 	private String textoLibre;
-	String item;
-	// public String getItem() {
-	// return item;
-	// }
-
-	// public void setItem(String item) {
-	// this.item = item;
-	// }
-
-	// private List<Item> items;
 	private List<POI> pois = new ArrayList<POI>();
-	
 	String ServicioAPI = "http://trimatek.org/Consultas/";
 
 	public BusquedaBean() {
 	}
-
-	// @ManagedProperty("#{poiService}")
-	// private PoiService service;
-
-	/*
-	 * @PostConstruct public void init() throws JSONException,
-	 * MalformedURLException, IOException, MessagingException { pois =
-	 * service.busquedaPois(textoLibre,usuario); }
-	 */
-
-	// public void setService(PoiService service) {
-	// this.service = service;
-	// }
 
 	public String getTextoLibre() {
 		return textoLibre;
@@ -75,27 +44,7 @@ public class BusquedaBean {
 		this.textoLibre = textoLibre;
 	}
 
-	// public List<Item> getItems() {
-	//
-	//
-	//
-	// return items;
-	// }
-	//
-	// public void setItems(List<Item> pois) {
-	// this.items = pois;
-	// }
-	//
-	// public PoiService getService() {
-	// return service;
-	// }
-
-	// public void add() {
-	// items.add(new Item());
-	//
-	// }
-
-	public void buscar() {	
+	public void buscar() {
 		pois.clear();
 		String username = ((String) FacesContext.getCurrentInstance().getExternalContext().getSessionMap()
 				.get("username"));
@@ -103,7 +52,7 @@ public class BusquedaBean {
 		Usuario usuario = Repositorio.getInstance().usuarios().getUsuarioByName(username);
 		ArrayList<POI> lstPOI = null;
 		try {
-			lstPOI = POI_ABMC.getInstance().buscar("", textoLibre, usuario.getId());
+			lstPOI = POI_ABMC.getInstance().buscar(ServicioAPI, textoLibre, usuario.getId());
 
 		} catch (JSONException | IOException | MessagingException e) {
 			// TODO Auto-generated catch block
